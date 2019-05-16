980badbcb99a1261b7315f37ce0af85437a88b95
Jeff Brown <39058795+daddyjab@users.noreply.github.com>
2019-05-16 00:19:45 -0500

    Added JavaScript code for population of Project table, collection of Technologies Used; Updated CSS

---
+++ static/js/portfolio_info.js
@@ -0,0 +1,99 @@
+// portfolioInfo
+// This object provides the project info for the list of projects
+portfolioInfo = [
+    {   projectName: 'GeoTweet',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub', linkURL: 'https://github.com/daddyjab/Project-2.io' },
+            {linkTag: 'Heroku', linkURL: 'https://geotweetapp.herokuapp.com'}
+            ],
+        technologies: [
+            'Python', 'Flask', 'SQLAlchemy', 'SQLite', 'Postgres', 'ETL (Extract/Translate/Load)', 'JavaScript', 'D3', 'Leaflet', 'Plotly', 'Machine Learning', 'Heroku'
+            ]
+    },
+
+    {   projectName: 'Earthquake',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub', linkURL: 'https://github.com/daddyjab/project_earthquake' },
+            {linkTag: 'Web', linkURL: 'https://www.northwestern.edu'}
+            ],
+        technologies: [
+            'JavaScript', 'D3', 'Leaflet'
+            ]
+    },
+
+    {   projectName: 'CitiBike',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub', linkURL: 'https://github.com/daddyjab/project_citibike' },
+            {linkTag: 'Tableau Public', linkURL: 'https://public.tableau.com/profile/jeffery.brown#!/vizhome/CitiBike_with_Paths_Idle_Time_JAB_v5/StoryCitiBike'}
+            ],
+        technologies: [
+            'Python', 'Data Munging', 'Tableau'
+            ]
+    },
+
+    {   projectName: 'I Was Poisoned',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub', linkURL: 'https://github.com/daddyjab/ETL_Project' }
+            ],
+        technologies: [
+            'Python', 'MongoDB', 'ETL (Extract/Translate/Load)', 'Web Scraping', 'BeautifulSoup'
+            ]
+    },
+
+    {   projectName: 'Biodiversity',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub', linkURL: 'https://github.com/daddyjab/project_biodiversity' }
+            ],
+        technologies: [
+            'Python', 'Flask', 'SQLAlchemy', 'SQLite', 'D3', 'Plotly'
+            ]
+    },
+
+    {   projectName: 'Health Risks',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub', linkURL: 'https://github.com/daddyjab/project_health_risks' }
+            ],
+        technologies: [
+            'JavaScript', 'D3'
+            ]
+    },
+
+    {   projectName: 'Weather by Latitude',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub - Analysis', linkURL: 'https://github.com/daddyjab/python-challenge/tree/master/WeatherPy'},
+            {linkTag: 'GitHub - Visualization', linkURL: 'https://github.com/daddyjab/project_latitude' }
+            ],
+        technologies: [
+            'Python', 'Matplotlib', 'HTML', 'CSS', 'Bootstrap'
+            ]
+    },
+
+
+    {   projectName: 'Mars Mission',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub', linkURL: 'https://github.com/daddyjab/project_mars_mission' }
+            ],
+        technologies: [
+            'Python', 'Flask', 'PyMongo', 'MongoDB', 'Web Scraping', 'BeautifulSoup', 'Splinter'
+            ]
+    },
+
+    {   projectName: 'Stock Reports',
+        description: 'brief description',
+        links: [
+            {linkTag: 'GitHub', linkURL: 'https://github.com/daddyjab/BootCampAssignments' }
+            ],
+        technologies: [
+            'Excel VBA'
+            ]
+    },
+    
+]
